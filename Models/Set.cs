20f570bd4b9ebbe847c5a125e0885661088018aa
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2022-02-20 14:40:49 +0100

    Preliminary set handling in ItemManager

---
+++ Models/Set.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MentalStates.Models
+{
+    public class Set
+    {
+        public int          SetId { get; set; }
+        public List<Item>   Items { get; set; }
+        public string       Name { get; set; }
+        public string       Description { get; set; }
+        public DateTime     Created { get; set; }
+        public DateTime     Updated { get; set; }
+    }
+}
